8ab646e85cc36351d0203f2eca076c251cc14d60
krakowiaka <72748944+krakowiaka@users.noreply.github.com>
2021-02-17 16:11:36 +0100

    Add files via upload

---
+++ SystemBazBadn/Main.java
@@ -0,0 +1,36 @@
+import kontroler.ZdarzenieGui;
+import model.Model;
+import widok.WidokGui;
+
+import javax.swing.*;
+
+/**
+ * Klasa startujaca aplikacje
+ */
+public class Main {
+    /**
+     * Metoda glowna
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+
+                //model
+                Model m = new Model();
+
+                m.ConnCreateInsert();
+
+                //widok
+                WidokGui w = new WidokGui(m);
+
+                //kontroler
+                ZdarzenieGui z = new ZdarzenieGui(w, m);
+                w.setController(z);
+
+                w.setVisible(true);
+            }
+        });
+    }
+}
